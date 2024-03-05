fb019e75b815d3b7fba0a8191cb1f50e8a5b6fb3
Guilherme Augusto <133021078+Guilherme-Augusto06@users.noreply.github.com>
2024-03-05 14:33:23 -0300

    Update inventarios.tsx

--- Screens/inventarios.tsx
+++ Screens/inventarios.tsx
@@ -6,14 +6,20 @@ import NavBar from '../components/navbar';
 import Footer from '../components/footer';
 import ButtonPlus from '../components/buttom1';
 import Objetos from '../components/objetos';
+import NavbarCadastro from '../components/navbarCadastroItens';
+import { Ionicons } from '@expo/vector-icons';
+import { Fontisto } from '@expo/vector-icons';
 
-export default function Teste() {
+export default function inventarios() {
   return (
   
 
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <NavBar color="#FFFFFF"/>
+      <NavbarCadastro color="#FFFFFF" colorText='black' text='Lista'icons={{
+        icon1: <Ionicons name="menu-sharp" size={24} color="black" />,
+        icon2: <Fontisto name="zoom" size={24} color="black" />
+      }}/>
       <ScrollView>
         <Objetos text="CADEIRA" color="#FFFFFF" number='956389'/>
         <Objetos text="CADEIRA" color="#FFFFFF" number='956389'/>
@@ -36,13 +42,9 @@ export default function Teste() {
         <Objetos text="CADEIRA" color="#FFFFFF" number='956389'/>
         <Objetos text="CADEIRA" color="#FFFFFF" number='956389'/>
         <Objetos text="CADEIRA" color="#FFFFFF" number='956389'/>
-
       </ScrollView>
-
       <Footer color='#FF0000'/>
-       
       <ButtonPlus/>
-
     </View>       
     
   );
